9b9ac75d338a0c5804f81dbf45a002a72777e5ff
Sudheer <sudheer@indigocube.co.za>
2020-04-03 17:08:40 +0200

    initial commit

---
+++ src/test/java/za/co/djsudz/NativeLibraryResourceIT.java
@@ -0,0 +1,9 @@
+package za.co.djsudz;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeLibraryResourceIT extends LibraryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
