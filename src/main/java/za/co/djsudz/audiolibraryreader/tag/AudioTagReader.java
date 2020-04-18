9e27f10afc93fa8e84056b1d99e8eb5b25e6936d
Sudheer <sudheer@indigocube.co.za>
2020-04-18 19:18:19 +0200

    Audio Tag Reader implementation and unit test

--- src/main/java/za/co/djsudz/audiolibraryreader/tag/AudioTagReader.java
+++ src/main/java/za/co/djsudz/audiolibraryreader/tag/AudioTagReader.java
@@ -11,8 +11,10 @@ import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.images.Artwork;
 
 /**
  * @author Sudheer
@@ -20,7 +22,7 @@ import org.jaudiotagger.tag.TagException;
  */
 public class AudioTagReader {
 
-	public Tag readAudioTag(File srcAudioFile) throws CannotReadException, IOException, 
+	public static Tag readAudioTag(File srcAudioFile) throws CannotReadException, IOException, 
 							TagException, ReadOnlyFileException, InvalidAudioFrameException {
 		if (srcAudioFile == null)
 			return null;
@@ -28,5 +30,45 @@ public class AudioTagReader {
 			AudioFile audioFile = AudioFileIO.read(srcAudioFile);
 			return audioFile.getTag();
 		}
+	}
+	
+	public static String getArtist(Tag tag) {		
+		return tag.getFirst(FieldKey.ARTIST);
+	}
+	
+	public static String getTitle(Tag tag) {
+		return tag.getFirst(FieldKey.TITLE);
+	}
+	
+	public static String getAlbum(Tag tag) {
+		return tag.getFirst(FieldKey.ALBUM);
+	}
+	
+	public static String getAlbumArtist(Tag tag) {
+		return tag.getFirst(FieldKey.ALBUM_ARTIST);
+	}
+	
+	public static String getGenre(Tag tag) {
+		return tag.getFirst(FieldKey.GENRE);
+	}
+	
+	public static String getComposer(Tag tag) {
+		return tag.getFirst(FieldKey.COMPOSER);
+	}
+	
+	public static String getComment(Tag tag) {
+		return tag.getFirst(FieldKey.COMMENT);
+	}
+	
+	public static String getYear(Tag tag) {
+		return tag.getFirst(FieldKey.YEAR);
+	}
+	
+	public static String getTrackNumber(Tag tag) {
+		return tag.getFirst(FieldKey.TRACK);
+	}
+	
+	public static Artwork getArtwork(Tag tag) {
+		return tag.getFirstArtwork();
 	}
 }
--- src/test/java/za/co/djsudz/audiolibraryreader/tag/AudioTagReaderTest.java
+++ src/test/java/za/co/djsudz/audiolibraryreader/tag/AudioTagReaderTest.java
@@ -7,56 +7,104 @@ import java.io.IOException;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
-import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
 import org.jaudiotagger.tag.images.Artwork;
 import org.jaudiotagger.tag.images.ArtworkFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class AudioTagReaderTest {
-
-	@Test
-	void testReadAudioTag() {
+	
+	private static Tag audioTag = null;
+	
+	@BeforeAll
+	private static void init() {
 		File sampleAudioFile = new File("src/test/resources/sampleAudioDirectory/sampleAudioMp3.mp3");
-		File sampleArtworkFile = new File("src/test/resources/sampleAudioDirectory/sample_album_art.jpg");
 		
 		//Check Sample Files Exist
 		assertTrue(sampleAudioFile.exists());
-		assertTrue(sampleArtworkFile.exists());
-		
-		AudioTagReader audioTagReader = new AudioTagReader();
-		
+				
 		try {
-			Tag audioTag = audioTagReader.readAudioTag(sampleAudioFile);
-			//Check Tag Field Count
-			assertEquals(10, audioTag.getFieldCount());
-			
-			//Check Tag Fields
-			assertTrue("Sample Artist".equals(audioTag.getFirst(FieldKey.ARTIST)));
-			assertTrue("Sample Title".equals(audioTag.getFirst(FieldKey.TITLE)));
-			assertTrue("Sample Album".equals(audioTag.getFirst(FieldKey.ALBUM)));
-			assertTrue("Sample Album Artist".equals(audioTag.getFirst(FieldKey.ALBUM_ARTIST)));
-			assertTrue("Sample Genre".equals(audioTag.getFirst(FieldKey.GENRE)));
-			assertTrue("Sample Composer".equals(audioTag.getFirst(FieldKey.COMPOSER)));
-			assertTrue("Sample Comment".equals(audioTag.getFirst(FieldKey.COMMENT)));
-			assertTrue("2020".equals(audioTag.getFirst(FieldKey.YEAR)));
-			assertTrue("01".equals(audioTag.getFirst(FieldKey.TRACK)));
-			
-			//Check Tag Artwork
-			Artwork sampleArtwork = ArtworkFactory.createArtworkFromFile(sampleArtworkFile);
-			Artwork tagArtwork = audioTag.getFirstArtwork();
-			assertEquals(sampleArtwork.getBinaryData().length, tagArtwork.getBinaryData().length);
-			assertEquals(sampleArtwork.getHeight(), tagArtwork.getHeight());
-			assertEquals(sampleArtwork.getWidth(), tagArtwork.getWidth());
-			assertTrue(sampleArtwork.getMimeType().equals(tagArtwork.getMimeType()));
-			
+			audioTag = AudioTagReader.readAudioTag(sampleAudioFile);
 		} catch (CannotReadException | IOException | TagException | ReadOnlyFileException
 				| InvalidAudioFrameException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	
 	}
 
+	@Test
+	void testReadAudioTag() {	
+		//Check Tag Field Count
+		assertEquals(10, audioTag.getFieldCount());			
+	}
+	
+	@Test
+	void testGetArtist() {
+		assertTrue("Sample Artist".equals(AudioTagReader.getArtist(audioTag)));
+	}
+	
+	@Test
+	void testGetTitle() {
+		assertTrue("Sample Title".equals(AudioTagReader.getTitle(audioTag)));
+	}
+	
+	@Test
+	void testGetAlbum() {
+		assertTrue("Sample Album".equals(AudioTagReader.getAlbum(audioTag)));
+	}
+	
+	@Test
+	void testGetAlbumArtist() {
+		assertTrue("Sample Album Artist".equals(AudioTagReader.getAlbumArtist(audioTag)));
+	}
+	
+	@Test
+	void testGetComposer() {
+		assertTrue("Sample Composer".equals(AudioTagReader.getComposer(audioTag)));
+	}
+	
+	@Test
+	void testGetGenre() {
+		assertTrue("Sample Genre".equals(AudioTagReader.getGenre(audioTag)));
+	}
+	
+	@Test
+	void testGetYear() {
+		assertTrue("2020".equals(AudioTagReader.getYear(audioTag)));
+	}
+	
+	@Test
+	void testGetTrackNumber() {
+		assertTrue("01".equals(AudioTagReader.getTrackNumber(audioTag)));
+	}
+	
+	@Test
+	void testGetComment() {
+		assertTrue("Sample Comment".equals(AudioTagReader.getComment(audioTag)));
+
+	}
+	
+	@Test
+	void testGetArtwork() {
+		File sampleArtworkFile = new File("src/test/resources/sampleAudioDirectory/sample_album_art.jpg");
+
+		assertTrue(sampleArtworkFile.exists());
+
+		//Check Tag Artwork
+		Artwork sampleArtwork;
+		try {
+			sampleArtwork = ArtworkFactory.createArtworkFromFile(sampleArtworkFile);
+			Artwork tagArtwork = AudioTagReader.getArtwork(audioTag);
+			assertEquals(sampleArtwork.getBinaryData().length, tagArtwork.getBinaryData().length);
+			assertEquals(sampleArtwork.getHeight(), tagArtwork.getHeight());
+			assertEquals(sampleArtwork.getWidth(), tagArtwork.getWidth());
+			assertTrue(sampleArtwork.getMimeType().equals(tagArtwork.getMimeType()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
