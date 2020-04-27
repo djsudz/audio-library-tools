a247aad5a086b4ee38af221acea066f2ac44f600
DJ Sudz <djsudz.co.za>
2020-04-27 17:26:38 +0200

    Created unit test for AudioTagUpdater

---
+++ src/test/java/za/co/djsudz/audiolibraryreader/tag/AudioTagUpdaterTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package za.co.djsudz.audiolibraryreader.tag;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Sudheer
+ *
+ */
+class AudioTagUpdaterTest {
+
+	/**
+	 * Test method for {@link za.co.djsudz.audiolibraryreader.tag.AudioTagUpdater#updateArtist(org.jaudiotagger.tag.Tag, java.lang.String)}.
+	 */
+	@Test
+	void testUpdateArtist() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link za.co.djsudz.audiolibraryreader.tag.AudioTagUpdater#updateAlbumArtist(org.jaudiotagger.tag.Tag, java.lang.String)}.
+	 */
+	@Test
+	void testUpdateAlbumArtist() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link za.co.djsudz.audiolibraryreader.tag.AudioTagUpdater#updateArtwork(org.jaudiotagger.tag.Tag, org.jaudiotagger.tag.images.Artwork)}.
+	 */
+	@Test
+	void testUpdateArtwork() {
+		fail("Not yet implemented");
+	}
+
+}
