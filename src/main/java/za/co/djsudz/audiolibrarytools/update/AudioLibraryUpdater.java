9b44ba3900e07c0e5937f8cfbbe5c1d75d62c5cc
DJ Sudz <djsudz.co.za>
2020-04-30 14:31:35 +0200

    Added AudioLibraryUpdater class

---
+++ src/main/java/za/co/djsudz/audiolibrarytools/update/AudioLibraryUpdater.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package za.co.djsudz.audiolibrarytools.update;
+
+import za.co.djsudz.audiolibrarytools.model.AudioLibrary;
+
+/**
+ * @author Sudheer
+ *
+ */
+public class AudioLibraryUpdater {
+	
+	public static void updateLibrary(AudioLibrary audioLibrary) {
+		
+	}
+
+}
