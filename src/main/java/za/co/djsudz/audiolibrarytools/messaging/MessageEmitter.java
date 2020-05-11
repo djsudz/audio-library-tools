f867150ca626324d63f6c87736b3915bd8c27b96
DJ Sudz <djsudz.co.za>
2020-05-11 21:41:40 +0200

    Message Streaming Implementation

---
+++ src/main/java/za/co/djsudz/audiolibrarytools/messaging/MessageEmitter.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package za.co.djsudz.audiolibrarytools.messaging;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+/**
+ * @author Sudheer
+ *
+ */
+@ApplicationScoped
+public class MessageEmitter {
+	@Inject
+	@Channel("alt-message-stream")
+	Emitter<String> emitter;
+	
+	public void semd(String message ) {
+		emitter.send(message);
+	}
+}
